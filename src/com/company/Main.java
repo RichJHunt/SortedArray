bd852cc92c6bc389719113397ebe9b18daccbb15
Demoraliza <51057587+Demoraliza@users.noreply.github.com>
2022-02-19 14:11:12 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    private static Object Integer;
+
+    public static void main(String[] args) {
+
+        int[] intArray = new int[]{ 5,6,7,2,4 };
+
+        sortArray(intArray);
+
+    }
+
+    private static int[] sortArray(int[] sortedArray) {
+        int[] baseArray = sortedArray;
+        
+
+
+        return baseArray;
+    }
+}
