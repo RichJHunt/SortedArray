44b5a21f87f8df2cb0b609dc10989ea3fea338d1
RichJHunt <51057587+RichJHunt@users.noreply.github.com>
2022-02-20 19:03:36 +0000

    Almost working

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,21 +2,32 @@ package com.company;
 
 public class Main {
 
-    private static Object Integer;
 
     public static void main(String[] args) {
 
         int[] intArray = new int[]{ 5,6,7,2,4 };
 
         sortArray(intArray);
-
+        for (int i : intArray) {
+            System.out.println(i);
+        }
     }
 
     private static int[] sortArray(int[] sortedArray) {
         int[] baseArray = sortedArray;
-        
+        int iteration = 0;
 
-
+            for (int i : baseArray) {
+                int currentValue = i;
+                int nextIteration = iteration+1;
+                int nextValue = baseArray[nextIteration];
+                    if (currentValue > nextValue) {
+                        int tempInt = baseArray[nextIteration];
+                        baseArray[nextIteration] = currentValue;
+                        baseArray[iteration] = tempInt;
+//                        iteration++;
+                    }
+            }
         return baseArray;
     }
 }
